d8295a732c63a3a85d895c418f5b6a71a05fefa0
Mohamed Hussin <mohamedhussin07@gmail.com>
2023-01-08 19:52:23 +0200

    create apis sanctum route grouping

--- SanctumAuth/app/Http/Controllers/Api/ProjectController.php
+++ SanctumAuth/app/Http/Controllers/Api/ProjectController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+
+    // CREATE PROJECT API
+    public function createProject(Request $request)
+    {
+        # code...
+    }
+
+    // LIST PROJECT API
+    public function listProject()
+    {
+        # code...
+    }
+
+    // SINGLE PROJECT API
+    public function singleProject($id)
+    {
+        # code...
+    }
+
+    // DELETE PROJECT API
+    public function deleteProject($id)
+    {
+        # code...
+    }
 }
--- SanctumAuth/app/Http/Controllers/Api/StudentController.php
+++ SanctumAuth/app/Http/Controllers/Api/StudentController.php
@@ -7,5 +7,24 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    // REGISTER API
+    public function register(Request $request)
+    {
+        # code...
+    }
+    // LOGIN API
+    public function login(Request $request)
+    {
+        # code...
+    }
+    //PROFILE API
+    public function profile()
+    {
+        # code...
+    }
+    // LOGOUT API
+    public function logout()
+    {
+        # code...
+    }
 }
