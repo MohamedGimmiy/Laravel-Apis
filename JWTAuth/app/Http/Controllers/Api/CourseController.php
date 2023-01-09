b2d835e410ea8b3e9dda6d972074a005a479641d
Mohamed Hussin <mohamedhussin07@gmail.com>
2023-01-09 15:40:48 +0200

    creating controllers scaffolding

---
+++ JWTAuth/app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    // COURSE ENROLLMENT API - POST
+    public function courseEnrollment(Request $request)
+    {
+        # code...
+    }
+
+    // Total Course Enrollment API - GET
+    public function totalCourses()
+    {
+        # code...
+    }
+
+    public function deleteCourse($id)
+    {
+        # code...
+    }
+}
---
+++ JWTAuth/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // USER REGISTER API - POST
+    public function register(Request $request)
+    {
+        # code...
+    }
+    // USER LOGIN API - POST
+    public function login(Request $request)
+    {
+        # code...
+    }
+    // USER PROFILE API - GET
+    public function profile()
+    {
+        # code...
+    }
+    // USER LOGOUT API - GET
+    public function logout()
+    {
+        # code...
+    }
+}
