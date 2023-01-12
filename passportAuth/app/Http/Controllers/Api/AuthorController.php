396f6048027776e9ef61a93750b42046e09aef6e
Mohamed Hussin <mohamedhussin07@gmail.com>
2023-01-12 17:46:07 +0200

    adding laravel passport

---
+++ passportAuth/app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    // REGISTER METHOD - POST
+    public function register(Request $request)
+    {
+        # code...
+    }
+    // LOGIN METHOD - POST
+    public function login(Request $request)
+    {
+        # code...
+    }
+    // PROFILE METHOD - GET
+    public function profile()
+    {
+        # code...
+    }
+    // LOGOUT METHOD - GET
+    public function logout()
+    {
+        # code...
+    }
+}
---
+++ passportAuth/app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // CREATE METHOD - POST
+    public function createBook(Request $request)
+    {
+
+    }
+    // LIST METHOD - GET
+    public function listBook()
+    {
+
+    }
+    // SINGLE METHOD - POST
+    public function singleBook($book_id)
+    {
+
+    }
+    // UPDATE METHOD - PUT
+    public function updateBook(Request $request, $book_id)
+    {
+
+    }
+    // DELETE METHOD - DELETE
+    public function deleteBook()
+    {
+
+    }
+}
